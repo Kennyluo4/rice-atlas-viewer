dffc5b6fdf2cfd3ccbcd262b2939f6d446d5890e
ziliangluo <kennyluo4@gmail.com>
2025-01-11 21:42:47 -0500

    update trajectory python script

--- pages/Trajectory_heatmap.py
+++ pages/Trajectory_heatmap.py
@@ -9,16 +9,15 @@ import os, subprocess, glob, atexit
 
 ## define function for generation figures using R
 @st.cache_data
-def generate_png_plot(species, feature, trajectory,candidateID_file):
+def generate_png_plot(species, feature, trajectory,candidateID):
     """
     Run the R script to generate the PNG plot.
     """
     import hashlib
 
-
     out_path = 'Rplot/temp'
 
-    cmd = ["Rscript", "Rplot/plot_trajectory_genes_TFs_motifs_ACRs_100824.R", str(species), str(feature),str(trajectory), str(candidateID_file), str(out_path)]
+    cmd = ["Rscript", "Rplot/plot_trajectory_genes_TFs_motifs_ACRs_only_heatmap_010925_HD.R", str(species), str(feature),str(trajectory), str(candidateID), str(out_path)]
     # test = ' '.join(cmd)
     # st.write('...Running: ', test)
     
@@ -54,48 +53,40 @@ def main():
     ###############################################
     ##                 Sidebar                  ###
     ###############################################
-    
-    
     st.sidebar.header('Plot Configuration')
     st.sidebar.markdown('## Please select a dataset to plot')
     
-    ## set the plot setting using form submission: 
-    with st.sidebar.form(key='Plot_config'):
-        species = st.selectbox('Species', [ '---Please choose---','Osativa', 'test2'])
-        feature = st.selectbox('Feature', [ '---Please choose---','Gene', 'TF', 'Mt', 'ACR'])
-        trajectory = st.selectbox('Trajectory',['---Please choose---','bud_ProcamPhloem', 'bud_ProcamXylem', 'Eseedling_SAMPhloem', 'Eseedling_SAMPhloemCC', 'Eseedling_SAMXylemPrecursor', 'crownroot_QCCortex', 'crownroot_QCMetaXylem', 'crownroot_QCProtoXylem','panicle_PBMSBMSMFM','panicle_PBMSMFM', 'semroot_QCCortex', 'semroot_QCMetaXylem', 'semroot_QCProtoXylem'])
-        feature_list = st.text_area('candidate feature ID (e.g. gene ID, motif ID, and ACRs)', 
-                                         'NAC011\nNAC005\nMYB46\nWRKY65')
-        # plot_h = st.slider('plot height(cm)', 1,30,10)
-        # plot_w = st.slider('plot width(cm)', 1,30,10)
-        
-        submitted = st.form_submit_button("Submit")
-        
-        # Clear cache when inputs change
-        if submitted:
-            st.cache_data.clear()  # Clear cache to avoid reusing old data
-            st.write('Generating heatmap...')
-            # st.write(f"Plot genes for {gene_list}")
+    ## set the plot settings: 
+    species = st.sidebar.selectbox('Species', [ '---Please choose---','Osativa', 'test2'])
+    trajectory = st.sidebar.selectbox('Trajectory',['---Please choose---','bud_ProcamPhloem', 'bud_ProcamXylem', 'Eseedling_SAMPhloem', 'Eseedling_SAMPhloemCC', 'Eseedling_SAMXylemPrecursor', 'crownroot_QCCortex', 'crownroot_QCMetaXylem', 'crownroot_QCProtoXylem','panicle_PBMSBMSMFM','panicle_PBMSMFM', 'semroot_QCCortex', 'semroot_QCMetaXylem', 'semroot_QCProtoXylem'])
+    # feature = st.selectbox('Feature', [ '---Please choose---','Gene', 'TF', 'Mt', 'ACR'])
+    feature = st.sidebar.radio('Feature', ['Gene', 'TF', 'Mt', 'ACR'], horizontal=True)
+    ## Retrive the featurelist after feature type selection
+    feature_file = f'Rplot/data/trajectory_features/opt_{feature}_feature_list.txt'
+    featureIDs = [id.strip() for id in open(feature_file).readlines()]
+    featureName = st.sidebar.selectbox('Enter feature name to highlight', ['', *featureIDs])
     
-    ## generate input files required by Rscript
-    if species != '---Please choose---' and feature != '---Please choose---' and trajectory != '---Please choose---':
-        # candidate_feature_file = f'ipt_target_species <- "{species}"\nipt_target_organ <- "{tissue}"\nplot_width <- {plot_w}\nplot_height <- {plot_h}\nFDR <- {fdr}'   
-        candidate_feature_file = f'Rplot/temp/input_3_users_provide_ID_fl_{feature}.txt'
-        with open(candidate_feature_file, 'w') as handle:
-            handle.write(feature_list)
-    # st.write('Updated plot configuration')
+    # plot_h = st.slider('plot height(cm)', 1,30,10)
+    # plot_w = st.slider('plot width(cm)', 1,30,10)
+
+    ## Create a submit button, plot only when click submit
+    with st.sidebar.form(key='my_form'):
+        st.write('Click to generate the plot')
+        submitted = st.form_submit_button("Submit")
+
     
     ###############################################
     ##                Main page                 ###
     ###############################################
     st.markdown('### Plot the trajectory')
-    if species != '---Please choose---' and feature != '---Please choose---' and trajectory != '---Please choose---':
-        fig = generate_png_plot(species, feature, trajectory, candidate_feature_file)
-        st.image(fig)
-    else:
-        st.error(':point_left: Please select the dataset and genes for plotting')
-    
-                             
+    if submitted:
+        st.cache_data.clear()  # Clear cache to avoid reusing old data
+        if species != '---Please choose---' and feature != '---Please choose---' and trajectory != '---Please choose---':
+            fig = generate_png_plot(species, feature, trajectory, featureName)
+            st.image(fig)
+        else:
+            st.error(':point_left: Please select the dataset and genes for plotting')
+        
 if __name__ == '__main__':
     main()
 
